8b7ffa03174d77a476d79b4c9d3a9dcaf809504f
yirano <tiffany.arellano@outlook.com>
2019-10-16 18:07:14 -0400

    Remove black background and color pixels on mouseover black

--- src/script.js
+++ src/script.js
@@ -5,21 +5,21 @@ let pixels;
 for (let i = 0; i < 44; i++) {
   pixels = document.createElement('div');
   pixels.classList.add = "pixels"
-  pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float: left;";
+  pixels.style.cssText = "width: 16px; height: 16px;  float: left;";
   canvas.appendChild(pixels);
-  for (let j = 0; j < 28; j++) {
+  for (let j = 0; j < 27; j++) {
     pixels = document.createElement('div');
     pixels.classList.add("pixels")
-    pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float:left;";
+    pixels.style.cssText = "width: 16px; height: 16px;  float:left;";
     canvas.appendChild(pixels);
   }
 }
 
 function entered(event) {
   console.log(event.srcElement.className);
-  event.target.style.cssText = "width: 16px; height: 16px; background-color: white; float:left;";
+  event.target.style.cssText = "width: 16px; height: 16px; background-color: black; float:left;";
 }
 
-document.addEventListener('click', entered)
+canvas.addEventListener("mouseover", entered)
 
 console.log(document.querySelectorAll('.pixels').length);
