edf2095646c5c675ab998c4a65545ad41373e1b1
yirano <tiffany.arellano@outlook.com>
2019-10-16 18:00:19 -0400

    Enable clicked boxes to turn white

--- src/script.js
+++ src/script.js
@@ -4,18 +4,22 @@ let pixels;
 
 for (let i = 0; i < 44; i++) {
   pixels = document.createElement('div');
+  pixels.classList.add = "pixels"
   pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float: left;";
   canvas.appendChild(pixels);
   for (let j = 0; j < 28; j++) {
     pixels = document.createElement('div');
+    pixels.classList.add("pixels")
     pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float:left;";
     canvas.appendChild(pixels);
   }
 }
 
 function entered(event) {
-  event.style.cssText = "width: 16px; height: 16px; background-color: #fff; float:left;";
-  console.log(event);
+  console.log(event.srcElement.className);
+  event.target.style.cssText = "width: 16px; height: 16px; background-color: white; float:left;";
 }
 
-console.log(pixels);
+document.addEventListener('click', entered)
+
+console.log(document.querySelectorAll('.pixels').length);
