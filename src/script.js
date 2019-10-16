bd09e9520cd7986d5766f76d3fbf3ca56c577c03
yirano <tiffany.arellano@outlook.com>
2019-10-16 17:40:10 -0400

    Fill canvas with pixels of 16x16

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
 let canvas = document.getElementById('canvas');
+let pixels;
 
-let black = function () {
-  console.log("over");
+
+for (let i = 0; i < 44; i++) {
+  pixels = document.createElement('div');
+  pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float: left;";
+  canvas.appendChild(pixels);
+  for (let j = 0; j < 28; j++) {
+    pixels = document.createElement('div');
+    pixels.style.cssText = "width: 16px; height: 16px; background-color: black; float:left;";
+    canvas.appendChild(pixels);
+  }
 }
 
-canvas.addEventListener('mousemove', black)
+function entered(event) {
+  event.style.cssText = "width: 16px; height: 16px; background-color: #fff; float:left;";
+  console.log(event);
+}
+
+console.log(pixels);
